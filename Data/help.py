0fbafab1399e0cdfb9643d90987d7c0ea480ffd7
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-29 23:35:29 +0530

    Create help.py

---
+++ Data/help.py
@@ -0,0 +1,98 @@
+from config import HANDLER as hl
+admins_msg = f"""
+๏ `{hl}promote` 
+๏ `{hl}demote`
+๏ `{hl}ban`
+๏ `{hl}unban`
+๏ `{hl}mute`
+๏ `{hl}unmute`
+๏ `{hl}setgcpfp`
+"""
+
+vc_msg = f"""
+๏ `{hl}startvc`
+"""
+
+telegraph_msg = f"""
+๏ `{hl}tgm
+"""
+
+own_msg = f"""
+๏ `{hl}block`
+๏ `{hl}unblock`
+๏ `{hl}setname`
+"""
+
+sangmeta_msg = f"""
+๏ `{hl}sg`
+"""
+
+join_msg = f"""
+๏ `{hl}join`
+๏ `{hl}leave`
+"""
+
+quotly_msg = f"""
+๏ `{hl}q`
+๏ `{hl}ani`
+"""
+
+web_msg = f"""
+๏ `{hl}webss`
+"""
+
+twitter_msg = f"""
+๏ `{hl}trupm`
+๏ `{hl}username`
+"""
+
+secret_msg = f"""
+๏ `{hl}wow`
+"""
+
+paste_msg = f"""
+๏ `{hl}paste`
+"""
+
+fake_msg = f"""
+๏ `{hl}ftyping`
+๏ `{hl}fvideo`
+๏ `{hl}fdocuments`
+๏ `{hl}faudio`
+๏ `{hl}fround`
+๏ `{hl}flocation`
+"""
+
+mantion_msg = f"""
+๏ `{hl}tagall`
+"""
+
+stats_msg = f"""
+๏ `{hl}mystats`
+"""
+
+carbon_msg = f"""
+๏ `{hl}carbon`
+"""
+
+google_msg = f"""
+๏ `{hl}google`
+"""
+
+eval_msg = f"""
+๏ `{hl}eval`
+"""
+
+id_msg = f"""
+๏ `{hl}id`
+"""
+
+fun_msg = f"""
+๏ `{hl}
+๏ `{hl}
+๏ `{hl}
+
+
+
+
+
