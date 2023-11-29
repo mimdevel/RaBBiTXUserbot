642c8b2f3559cf695c06854f20af0d5103653632
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-29 13:09:03 +0530

    Create wink.py

---
+++ bunny/modules/Wink/wink.py
@@ -0,0 +1,17 @@
+import asyncio
+import requests
+from pyrogram import *
+from pyrogram import Client, filters
+from pyrogram.types import *
+from pyrogram.types import Message
+from config import HANDLER as hl
+from powers.powers.basic import edit_or_reply, get_text
+from bunny.core.clients import bunny as Client
+
+@Client.on_message(filters.command("wink", hl) & filters.me)
+async def wink(client: Client, message: Message):
+    hmm_s = "https://some-random-api.ml/animu/wink"
+    r = requests.get(url=hmm_s).json()
+    image_s = r["link"]
+    await client.send_video(message.chat.id, image_s)
+    await message.delete()
