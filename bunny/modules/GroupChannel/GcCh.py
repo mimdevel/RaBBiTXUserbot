e73d45f578c8c6ae67a19a82cfa52903dec2290a
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-29 12:21:14 +0530

    Update GcCh.py

--- bunny/modules/GroupChannel/GcCh.py
+++ bunny/modules/GroupChannel/GcCh.py
@@ -30,15 +30,13 @@ async def gcch(client: Client, message: Message):
 ╰๏ 𝐃ᴏɴ'ᴛ 𝐔sᴇ 𝐒ʟᴀɴɢ 𝐋ᴀɴɢᴜᴀɢᴇ 𝐖ʜɪʟᴇ 𝐓ᴀʟᴋɪɴɢ 𝐈ɴ 𝐆ʀᴏᴜᴘ"""
     if group_type == "group": 
         _id = await client.create_supergroup(group_name, fuk)
-        link = await client.get_chat(_id["id"])
         await bunny.edit(
-            f"__successfully created your Group as: [{group_name}]({link['invite_link']})__",
+            f"__successfully created your Group..!!__",
             disable_web_page_preview=True,
         )
     elif group_type == "channel":  
         _id = await client.create_channel(group_name, fuk)
-        link = await client.get_chat(_id["id"])
         await bunny.edit(
-            f"__successfully created your channel as: [{group_name}]({link['invite_link']})__",
+            f"__successfully created your channel..!!__",
             disable_web_page_preview=True,
         )
