1215edd7317b0eaa4b6c5667ba4b703111bafe60
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-30 13:49:20 +0530

    Create help.py

---
+++ bunny/modules/Help/help.py
@@ -0,0 +1,27 @@
+helpmenu = [
+    [
+        InlineKeyboardButton(text="ᴀᴅᴍɪɴs",callback_data="help"),
+        InlineKeyboardButton(text="ᴠᴄ", callback_data="vc"),
+        InlineKeyboardButton(text="ᴏᴡɴ", callback_data="own"),
+    ],
+    [
+        InlineKeyboardButton(text="ʜɪsᴛᴏʀʏ", callback_data="sang"),
+        InlineKeyboardButton(text="sᴇᴄʀᴇᴛ", callback_data="secret"),
+        InlineKeyboardButton(text="ᴄʟᴏɴᴇ", callback_data="clone"),
+    ],
+    [
+        InlineKeyboardButton(text="ᴛᴇʟᴇɢʀᴀᴘʜ", callback_data="home"),
+        InlineKeyboardButton(text="ᴘᴍᴘᴇʀᴍɪᴛ", callback_data="h"),
+        InlineKeyboardButton(text="ᴊᴏɪɴ", callback_data="sudo"),
+    ],
+    [
+        InlineKeyboardButton(text="ᴡᴇʙsɪᴛᴇ", callback_data="sudo"),
+        InlineKeyboardButton(text="ᴛᴡᴇᴇᴛ", callback_data="sudo"),
+        InlineKeyboardButton(text="ғᴀᴋᴇ", callback_data="sudo"),
+    ],
+    [
+       InlineKeyboardButton(text="ǫᴜᴏᴛʟʏ", callback_data="sudo"),
+       InlineKeyboardButton(text="ᴘᴀsᴛᴇ", callback_data="sudo"),
+       InlineKeyboardButton(text="sᴛᴀᴛs", callback_data="sudo"),
+    ],
+]
