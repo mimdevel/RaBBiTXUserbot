bbda3ff03e4751aa01dc56cb41e79e771d3ed46b
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-30 09:00:27 +0530

    Update help.py

--- Data/help.py
+++ Data/help.py
@@ -1,124 +1,121 @@
 from config import HANDLER as hl
 
 admins_msg = f"""
-๏ `{hl}promote` 
-๏ `{hl}demote`
-๏ `{hl}ban`
-๏ `{hl}unban`
-๏ `{hl}mute`
-๏ `{hl}unmute`
-๏ `{hl}setgcpfp`
+๏ `{hl}promote` »
+๏ `{hl}demote` »
+๏ `{hl}ban` »
+๏ `{hl}unban` »
+๏ `{hl}mute` »
+๏ `{hl}unmute` »
+๏ `{hl}setgcpfp` »
 """
 
 vc_msg = f"""
-๏ `{hl}startvc`
+๏ `{hl}startvc` »
 """
 
 telegraph_msg = f"""
-๏ `{hl}tgm
+๏ `{hl}tgm »
 """
 
 own_msg = f"""
-๏ `{hl}block`
-๏ `{hl}unblock`
-๏ `{hl}setname`
+๏ `{hl}block` »
+๏ `{hl}unblock` »
+๏ `{hl}setname` »
 """
 
 sangmeta_msg = f"""
-๏ `{hl}sg`
+๏ `{hl}sg` »
 """
 
 join_msg = f"""
-๏ `{hl}join`
-๏ `{hl}leave`
+๏ `{hl}join` »
+๏ `{hl}leave` »
 """
 
 quotly_msg = f"""
-๏ `{hl}q`
-๏ `{hl}ani`
+๏ `{hl}q` »
+๏ `{hl}ani` »
 """
 
 web_msg = f"""
-๏ `{hl}webss`
+๏ `{hl}webss` »
 """
 
 twitter_msg = f"""
-๏ `{hl}trupm`
-๏ `{hl}username`
+๏ `{hl}trupm` »
+๏ `{hl}username` »
 """
 
 secret_msg = f"""
-๏ `{hl}wow`
+๏ `{hl}wow` »
 """
 
 paste_msg = f"""
-๏ `{hl}paste`
+๏ `{hl}paste` »
 """
 
 fake_msg = f"""
-๏ `{hl}ftyping`
-๏ `{hl}fvideo`
-๏ `{hl}fdocuments`
-๏ `{hl}faudio`
-๏ `{hl}fround`
-๏ `{hl}flocation`
+๏ `{hl}ftyping` »
+๏ `{hl}fvideo` »
+๏ `{hl}fdocuments` »
+๏ `{hl}faudio` »
+๏ `{hl}fround` »
+๏ `{hl}flocation` »
 """
 
 mantion_msg = f"""
-๏ `{hl}tagall`
+๏ `{hl}tagall` »
 """
 
 stats_msg = f"""
-๏ `{hl}mystats`
+๏ `{hl}mystats` »
 """
 
 carbon_msg = f"""
-๏ `{hl}carbon`
+๏ `{hl}carbon` »
 """
 
 google_msg = f"""
-๏ `{hl}google`
+๏ `{hl}google` »
 """
 
 eval_msg = f"""
-๏ `{hl}eval`
+๏ `{hl}eval` »
 """
 
 id_msg = f"""
-๏ `{hl}id`
+๏ `{hl}id` »
 """
 
 fun_msg = f"""
-๏ `{hl}hug`
-๏ `{hl}wink`
-๏ `{hl}fuck`
+๏ `{hl}hug` »
+๏ `{hl}wink` »
+๏ `{hl}fuck` »
 """
 
 create_msg = f"""
-๏ `{hl}create group RaBBiTX`
-๏ `{hl}create channel RaBBiTX`
+๏ `{hl}create group RaBBiTX` »
+๏ `{hl}create channel RaBBiTX` »
 """
 
 invite_msg = f"""
-๏ `{hl}invitelink`
-๏ `{hl}invite`
-๏ `{hl}inviteall
+๏ `{hl}invitelink` »
+๏ `{hl}invite` »
+๏ `{hl}inviteall »
 """
 
 clone_msg = f"""
-๏ `{hl}clone`
-๏ `{hl}revert`
+๏ `{hl}clone` »
+๏ `{hl}revert` »
 """
 
 extra_msg = f"""
-๏ `{hl}ping`
-๏ `{hl}alive`
+๏ `{hl}ping` »
+๏ `{hl}alive` »
 """
 
 pm_msg = f"""
-๏ `{hl}pmpermit on`
-๏ `{hl}pmpermit off`
+๏ `{hl}pmpermit on` »
+๏ `{hl}pmpermit off` »
 """
-
-
-
